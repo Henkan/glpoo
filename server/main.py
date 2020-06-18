bf92cf7e59138167f5b55f77b85888c2fe386a2a
Sylvain Roncoroni <sylvain.ronco@gmail.com>
2020-06-18 11:57:18 +0200

    Add Sports entrypoint in Server

--- server/main.py
+++ server/main.py
@@ -2,6 +2,7 @@
 from flask import Flask
 from model.database import DatabaseEngine
 from vue.member_resource import member_resource
+from vue.sport_resource import sport_resource
 
 
 def main():
@@ -14,6 +15,7 @@ def main():
     # Start api
     app = Flask(__name__)
     app.register_blueprint(member_resource)
+    app.register_blueprint(sport_resource)
     app.run(host="0.0.0.0", port=5000)
 
 
