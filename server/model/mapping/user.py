b129b61c12d7c9c64f056e21acf93a19668aa87e
Sylvain Roncoroni <sylvain.ronco@gmail.com>
2020-06-19 10:56:21 +0200

    Add simple authentication

--- server/model/mapping/user.py
+++ server/model/mapping/user.py
@@ -2,15 +2,17 @@ from model.mapping import Base
 import uuid
 import hashlib
 
-from sqlalchemy import Column, String, Integer
+from sqlalchemy import Column, String, Boolean, UniqueConstraint
 
 
 class User(Base):
     __tablename__ = 'user'
+    __table_args__ = (UniqueConstraint('username'),)
 
     id = Column(String(36), default=str(uuid.uuid4()), primary_key=True)
     username = Column(String(50), nullable=False)
     password_hash = Column(String(64))
+    admin = Column(Boolean, default=False, nullable=False)
 
     def __repr__(self):
         return "<User (%s, %s)>" % (self.username, self.password_hash)
@@ -19,7 +21,8 @@ class User(Base):
         data = {
             "id": self.id,
             "username": self.username,
-            "password_hash": self.password_hash
+            "password_hash": self.password_hash,
+            "admin": self.admin
         }
         return data
 
@@ -27,4 +30,4 @@ class User(Base):
         self.password_hash = hashlib.sha256(password.encode()).hexdigest()
 
     def check_password(self, password):
-        return self.password_hash == hashlib.sha256(password.encore()).hexdigest()
+        return self.password_hash == hashlib.sha256(password.encode()).hexdigest()
